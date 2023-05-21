45b296b95795c96bff8ba860776d63b8f791bb3e
Pol <polgil24@hotmail.com>
2023-05-21 15:16:25 +0200

    Add styles to Layout Component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import LayoutStyled from "./LayoutStyled";
 
 const Layout = (): JSX.Element => {
   return (
-    <>
+    <LayoutStyled className="container">
       <h1>Social network</h1>
       <Outlet />
-    </>
+    </LayoutStyled>
   );
 };
 
---
+++ src/components/Layout/LayoutStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    text-align: center;
+  }
+`;
+export default LayoutStyled;
