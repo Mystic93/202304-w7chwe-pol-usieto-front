6bbbf515c5b7c28ec4a875d2163cc23450c09fe5
Pol <polgil24@hotmail.com>
2023-05-21 17:23:43 +0200

    Add LoginForm Component

---
+++ src/components/LoginForm/LoginFormStyled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  padding: 10px;
+
+  .loginform-label {
+    padding: 5px;
+  }
+
+  .loginform-input {
+    padding: 5px;
+  }
+`;
+export default LoginFormStyled;
